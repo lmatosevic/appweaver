969cf30fe360e85c376085d31e3e7b168f4df1ce
lmatosevic <lukamatosevic5@gmail.com>
2026-02-05 18:13:18 +0100

    fix: add initial ResourceService type

--- packages/common/types/service.ts
+++ packages/common/types/service.ts
@@ -1,3 +1,16 @@
 export type ResourceService = {
   name?: string;
-}
+  beforeFind?: (id: any) => any;
+  beforeQuery?: (filter: any) => any;
+  beforeAggregate?: (filter: any) => any;
+  beforeCreate?: (data: any) => any;
+  beforeUpdate?: (id: any, data: any) => any;
+  beforeDelete?: (id: any) => any;
+  afterFind?: (resource: any) => any;
+  afterQuery?: (response: any) => any;
+  afterAggregate?: (response: any) => any;
+  afterCreate?: (resource: any) => any;
+  afterUpdate?: (resource: any) => any;
+  afterDelete?: (resource: any) => any;
+  textSearch?: (input: string) => any;
+};
