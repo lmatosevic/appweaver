a57ceb92d494f09f5f1aefb129f61814ee5a94ff
lmatosevic <lukamatosevic5@gmail.com>
2026-04-29 21:19:26 +0200

    fix: add .npmignore files for all packages

--- tools/constants.js
+++ tools/constants.js
@@ -2,6 +2,6 @@ const moduleName = '@appweaver';
 const packagesDir = 'packages';
 const nodeModulesDir = 'node_modules';
 const distDir = 'dist';
-const moduleFiles = ['package.json', 'README.md', 'LICENSE'];
+const moduleFiles = ['package.json', 'README.md', 'LICENSE', '.npmignore'];
 
 export { moduleName, packagesDir, nodeModulesDir, distDir, moduleFiles };
