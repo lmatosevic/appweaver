1cd19ff4a1f857f8378a6be6b0a9f24616c2ffcc
lmatosevic <lukamatosevic5@gmail.com>
2026-08-01 14:38:58 +0200

    fix: change File checksum maxLength to 127

--- packages/core/storage/resources/file/model.ts
+++ packages/core/storage/resources/file/model.ts
@@ -27,7 +27,7 @@ export default createModel({
     },
     checksum: {
       type: 'string',
-      maxLength: 128,
+      maxLength: 127,
       example:
         '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
     },
